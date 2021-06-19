501401b0b40ddbfb41373d0d9026b82f99674be5
vngarg <sngargrsd@gmail.com>
2021-06-20 00:34:56 +0530

    Added the package

---
+++ dist/index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+require("./styles.css");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const Loader = () => {
+  return /*#__PURE__*/_react.default.createElement("div", {
+    className: "loader"
+  });
+};
+
+var _default = Loader;
+exports.default = _default;
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Loader from './lib';
+
+function App() {
+  return (
+    <Loader />
+  );
+}
+
+export default App;
